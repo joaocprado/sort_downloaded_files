0b6a16917f254bcbf17fe0779b0ea8e174632ee5
Joao Carlos Prado <joaocarlos@profectum.com.br>
2024-05-08 18:07:44 -0300

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/joaocprado/sort_downloaded_files
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+)
+
+func main() {
+	homeDir, _ := os.UserHomeDir()
+	downloadDir := fmt.Sprintf("%s/downloads/complete", homeDir)
+	downloadDir = "/home/joao/code_project/sort_downloaded_files/downloads" // Test
+	filesInDir, _ := os.ReadDir(downloadDir)
+	subspleaseRegex, _ := regexp.Compile(`\[SubsPlease\]`)
+	ytsRegex, _ := regexp.Compile(`\[YTS\.\w+\]`)
+	for _, file := range filesInDir {
+		if subspleaseRegex.MatchString(file.Name()) {
+			fmt.Println(file.Name())
+		}
+		if ytsRegex.MatchString(file.Name()) {
+			fmt.Println(file.Name())
+		}
+	}
+}
