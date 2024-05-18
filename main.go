16a8fc96ecefef9d76555d0385de8c3d0fa87d4f
Joao Carlos Prado <joaocarlos@profectum.com.br>
2024-05-18 14:40:02 -0300

    add move to folder and log when error or success

--- main.go
+++ main.go
@@ -2,27 +2,49 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path"
 	"regexp"
 )
 
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func logMove(oldPath string, newPath string) {
+	message := fmt.Sprintf("File moved: %s -> %s", oldPath, newPath)
+	fmt.Println(message)
+}
+
 func main() {
 	homeDir, _ := os.UserHomeDir()
-	downloadDir := fmt.Sprintf("%s/downloads/complete", homeDir)
-	// downloadDir = "/home/joao/code_project/sort_downloaded_files/downloads" // Test
+	mediaDir := path.Join(homeDir, "media")
+	animeDir := path.Join(mediaDir, "anime")
+	movieDir := path.Join(mediaDir, "movie")
+	downloadDir := path.Join(homeDir, "downloads", "complete")
 	filesInDir, _ := os.ReadDir(downloadDir)
 	subspleaseRegex, _ := regexp.Compile(`\[SubsPlease\]`)
 	ytsRegex, _ := regexp.Compile(`\[YTS\.\w+\]`)
 	tgxRegex, _ := regexp.Compile(`\[TGx\]`)
 	for _, file := range filesInDir {
+		oldPath := path.Join(downloadDir, file.Name())
+		fmt.Println(oldPath)
 		if subspleaseRegex.MatchString(file.Name()) {
 			fmt.Println(file.Name())
+			newPath := path.Join(animeDir, file.Name())
+			err := os.Rename(oldPath, newPath)
+			checkErr(err)
+			logMove(oldPath, newPath)
 		}
-		if ytsRegex.MatchString(file.Name()) {
+		if ytsRegex.MatchString(file.Name()) || tgxRegex.MatchString(file.Name()) {
 			fmt.Println(file.Name())
-		}
-		if tgxRegex.MatchString(file.Name()) {
-			fmt.Println(file.Name())
+			newPath := path.Join(movieDir, file.Name())
+			err := os.Rename(oldPath, newPath)
+			checkErr(err)
+			logMove(oldPath, newPath)
 		}
 	}
 }
